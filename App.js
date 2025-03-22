cca636aa071ce31cebe584d9cb9fb1b611fc2b6e
maheshperla1518 <maheshperla015@gmail.com>
2025-03-22 20:11:17 +0530

    end of episode 2 files

--- App.js
+++ App.js
@@ -1,25 +1,6 @@
-// var h1_tag = React.createElement("h1",{className:"para1"},"hello mahesh from React!");
-// console.log(h1_tag); //object
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// var root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(h1_tag);
-
-//nested elements structure
-
-// var parent = React.createElement(
-//     "div",
-//      { id: "parent" }, [
-//         React.createElement("div", { id: "child1" }, [
-//             React.createElement("h1", {}, "this is H1 tag"),
-//             React.createElement("h2", {}, "this is H2 tag")
-//         ]),
-//         React.createElement("div", { id: "child2" }, [
-//             React.createElement("h1", {}, "this is H1 tag"),
-//             React.createElement("h2", {}, "this is H2 tag")
-//         ])
-//      ]
-// );
-// console.log(parent); //object
 
 // var root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(parent);
@@ -29,15 +10,9 @@
 // div>>div>>ptag
 var div_tag = React.createElement("div",{className:"alpha"},
     React.createElement("div",{className:"div2"},
-        React.createElement("p",{className:"paragrapgh",},'hello mahesh perla')))
+        React.createElement("p",{className:"paragrapgh",},
+            'this is h tag perla and continue your lession 🚀')))
 
 var root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(div_tag);
-
-
-
-
-
-
-
 
